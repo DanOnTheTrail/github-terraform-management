e9ff13e61ef4e932743f225bc157474385536dec
Dan Cook <5843830+DanOnTheTrail@users.noreply.github.com>
2023-04-22 22:32:29 -0400

    feat: create a single GitHub repo

---
+++ src/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  cloud {
+    organization = "cherry-space"
+
+    workspaces {
+      name = "github-terraform-management"
+    }
+  }
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {}
