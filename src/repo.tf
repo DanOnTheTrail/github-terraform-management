807e87631bcfc3016792b4cd74b34d094eea1ae7
Dan Cook <5843830+DanOnTheTrail@users.noreply.github.com>
2023-04-23 01:29:41 -0400

    feat: support properties for each repo

--- src/repo.tf
+++ src/repo.tf
@@ -6,13 +6,32 @@ resource "github_repository" "example" {
 }
 
 locals {
-  repos_config = yamldecode(file("${path.module}/../config/repos.yml"))
+  repos = {
+    for repo in yamldecode(file("${path.module}/../config/repos.yml")).repos :
+    repo.name => repo
+  }
 }
 
 resource "github_repository" "managed_repos" {
-  for_each = toset(local.repos_config.repos)
+  for_each = local.repos
 
-  name        = each.value
-  description = "foo"
-  visibility  = "private"
+  name                        = each.value.name
+  description                 = each.value.description
+  visibility                  = each.value.visibility
+  allow_auto_merge            = each.value.allow_auto_merge
+  allow_merge_commit          = each.value.allow_merge_commit
+  allow_rebase_merge          = each.value.allow_rebase_merge
+  allow_squash_merge          = each.value.allow_squash_merge
+  delete_branch_on_merge      = each.value.delete_branch_on_merge
+  archived                    = each.value.archived
+  squash_merge_commit_message = each.value.squash_merge_commit_message
+  squash_merge_commit_title   = each.value.squash_merge_commit_title
+  allow_update_branch         = each.value.allow_update_branch
+
+  vulnerability_alerts = true
+  archive_on_destroy   = true
+  has_discussions      = false
+  has_issues           = false
+  has_projects         = false
+  has_wiki             = false
 }
